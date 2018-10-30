eaf50074579d3263542c1c09e7a5f185554f838b
Kiran Belekar <kiran.belekar@acellere.com>
2018-10-30 16:26:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass_3
+{
+    public partial class Class1
+    {
+        partial void PartialMethod(int val);
+
+          public void MyPartialClass()
+            {
+            
+            }      
+
+         public void Method2(int val)
+            {
+                 Console.WriteLine(val);
+            }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass_3
+{
+     public partial class Class1
+    {
+        public void Method1(int val)
+        {
+            Console.WriteLine(val);
+        }
+
+        partial void PartialMethod(int val)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
